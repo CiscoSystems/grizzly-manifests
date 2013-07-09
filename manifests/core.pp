e774846f377a960ba4699659a59c53ae49794b94
Chris Ricker <chricker@cisco.com>
2013-07-09 06:42:37 -0700

    Allow setting of quantum quota variables
    Resolve Bug #1194644. Expose quantum quota variables through site.pp
    since many deployments will need them adjusted.

--- manifests/core.pp
+++ manifests/core.pp
@@ -151,55 +151,63 @@ node os_base inherits base {
 
 class control(
   $tunnel_ip,
-  $public_address          = $::controller_node_public,
+  $public_address                    = $::controller_node_public,
   # network
-  $internal_address        = $::controller_node_internal,
+  $internal_address                  = $::controller_node_internal,
   # by default it does not enable multi-host mode
-  $multi_host              = $::multi_host,
-  $verbose                 = $::verbose,
-  $auto_assign_floating_ip = $::auto_assign_floating_ip,
-  $mysql_root_password     = $::mysql_root_password,
-  $admin_email             = $::admin_email,
-  $admin_password          = $::admin_password,
-  $keystone_db_password    = $::keystone_db_password,
-  $keystone_admin_token    = $::keystone_admin_token,
-  $glance_db_password      = $::glance_db_password,
-  $glance_user_password    = $::glance_user_password,
+  $multi_host                        = $::multi_host,
+  $verbose                           = $::verbose,
+  $auto_assign_floating_ip           = $::auto_assign_floating_ip,
+  $mysql_root_password               = $::mysql_root_password,
+  $admin_email                       = $::admin_email,
+  $admin_password                    = $::admin_password,
+  $keystone_db_password              = $::keystone_db_password,
+  $keystone_admin_token              = $::keystone_admin_token,
+  $glance_db_password                = $::glance_db_password,
+  $glance_user_password              = $::glance_user_password,
 
   # TODO this needs to be added
-  $glance_backend          = $::glance_backend,
+  $glance_backend                    = $::glance_backend,
 
-  $nova_db_password        = $::nova_db_password,
-  $nova_user_password      = $::nova_user_password,
-  $rabbit_password         = $::rabbit_password,
-  $rabbit_user             = $::rabbit_user,
+  $nova_db_password                  = $::nova_db_password,
+  $nova_user_password                = $::nova_user_password,
+  $rabbit_password                   = $::rabbit_password,
+  $rabbit_user                       = $::rabbit_user,
   # TODO deprecated
-  #export_resources        = false,
+  #export_resources                  = false,
 
   ######### quantum variables #############
   # need to set from a variable
   # database
-  $db_host                 = $::controller_node_address,
-  $quantum_db_password     = $quantum_db_password,
-  $quantum_db_name         = 'quantum',
-  $quantum_db_user         = 'quantum',
+  $db_host                           = $::controller_node_address,
+  $quantum_db_password               = $quantum_db_password,
+  $quantum_db_name                   = 'quantum',
+  $quantum_db_user                   = 'quantum',
   # enable quantum services
-  $enable_dhcp_agent       = $true,
-  $enable_l3_agent         = $true,
-  $enable_metadata_agent   = $true,
+  $enable_dhcp_agent                 = $true,
+  $enable_l3_agent                   = $true,
+  $enable_metadata_agent             = $true,
   # Metadata Configuration
-  $metadata_shared_secret  = 'secret',
+  $metadata_shared_secret            = 'secret',
   # ovs config
-  $ovs_local_ip            = $tunnel_ip,
-  $bridge_interface        = $::external_interface,
-  $enable_ovs_agent        = true,
+  $ovs_local_ip                      = $tunnel_ip,
+  $bridge_interface                  = $::external_interface,
+  $enable_ovs_agent                  = true,
   # Keystone
-  $quantum_user_password   = $::quantum_user_password,
+  $quantum_user_password             = $::quantum_user_password,
   # horizon
-  $secret_key              = 'super_secret',
+  $secret_key                        = 'super_secret',
   # cinder
-  $cinder_user_password    = $::cinder_user_password,
-  $cinder_db_password      = $::cinder_db_password,
+  $cinder_user_password              = $::cinder_user_password,
+  $cinder_db_password                = $::cinder_db_password,
+  # Quantum quotas
+  $quantum_quota_network             = $::quantum_quota_network,
+  $quantum_quota_subnet              = $::quantum_quota_subnet,
+  $quantum_quota_port                = $::quantum_quota_port,
+  $quantum_quota_router              = $::quantum_quota_router,
+  $quantum_quota_floatingip          = $::quantum_quota_floatingip,
+  $quantum_quota_security_group      = $::quantum_quota_security_group,
+  $quantum_quota_security_group_rule = $::quantum_quota_security_group_rule,
 )
 {
 
@@ -265,6 +273,16 @@ class control(
 
   class { "naginator::control_target": }
 
+  class { "quantum::quota":
+    quota_network             => $quantum_quota_network,
+    quota_subnet              => $quantum_quota_subnet,
+    quota_port                => $quantum_quota_port,
+    quota_router              => $quantum_quota_router,
+    quota_floatingip          => $quantum_quota_floatingip,
+    quota_security_group      => $quantum_quota_security_group,
+    quota_security_group_rule => $quantum_quota_security_group_rule,
+  }
+
 }
 
 
@@ -272,39 +290,47 @@ class compute(
   $internal_ip,
   $tunnel_ip,
   # keystone
-  $db_host               = $::controller_node_internal,
-  $keystone_host         = $::controller_node_internal,
-  $quantum_host          = $::controller_node_internal,
-  $internal_address      = $internal_ip,
-  $libvirt_type          = $::libvirt_type,
-  $multi_host            = $::multi_host,
+  $db_host                           = $::controller_node_internal,
+  $keystone_host                     = $::controller_node_internal,
+  $quantum_host                      = $::controller_node_internal,
+  $internal_address                  = $internal_ip,
+  $libvirt_type                      = $::libvirt_type,
+  $multi_host                        = $::multi_host,
   # rabbit
-  $rabbit_host           = $::controller_node_internal,
-  $rabbit_password       = $::rabbit_password,
-  $rabbit_user           = $::rabbit_user,
+  $rabbit_host                       = $::controller_node_internal,
+  $rabbit_password                   = $::rabbit_password,
+  $rabbit_user                       = $::rabbit_user,
   # nova
-  $nova_user_password    = $::nova_user_password,
-  $nova_db_password      = $::nova_db_password,
-  $glance_api_servers    = "${::controller_node_internal}:9292",
-  $vncproxy_host         = $::controller_node_public,
-  $vnc_enabled           = true,
+  $nova_user_password                = $::nova_user_password,
+  $nova_db_password                  = $::nova_db_password,
+  $glance_api_servers                = "${::controller_node_internal}:9292",
+  $vncproxy_host                     = $::controller_node_public,
+  $vnc_enabled                       = true,
   # cinder parameters
-  $cinder_db_password    = $::cinder_db_password,
-  $manage_volumes        = true,
-  $volume_group          = 'cinder-volumes',
-  $setup_test_volume     = true,
+  $cinder_db_password                = $::cinder_db_password,
+  $manage_volumes                    = true,
+  $volume_group                      = 'cinder-volumes',
+  $setup_test_volume                 = true,
   # quantum config
-  $quantum	         = true,
-  $quantum_user_password = $::quantum_user_password,
+  $quantum	                         = true,
+  $quantum_user_password             = $::quantum_user_password,
   # Quantum OVS
-  $enable_ovs_agent      = true,
-  $ovs_local_ip          = $tunnel_ip,
+  $enable_ovs_agent                  = true,
+  $ovs_local_ip                      = $tunnel_ip,
   # Quantum L3 Agent
-  $enable_l3_agent       = false,
-  $enable_dhcp_agent     = false,
+  $enable_l3_agent                   = false,
+  $enable_dhcp_agent                 = false,
+  # Quantum quotas
+  $quantum_quota_network             = $::quantum_quota_network,
+  $quantum_quota_subnet              = $::quantum_quota_subnet,
+  $quantum_quota_port                = $::quantum_quota_port,
+  $quantum_quota_router              = $::quantum_quota_router,
+  $quantum_quota_floatingip          = $::quantum_quota_floatingip,
+  $quantum_quota_security_group      = $::quantum_quota_security_group,
+  $quantum_quota_security_group_rule = $::quantum_quota_security_group_rule,
   # general
-  $enabled               = true,
-  $verbose               = $::verbose,
+  $enabled                           = true,
+  $verbose                           = $::verbose,
 )
 {
 
@@ -346,6 +372,16 @@ class compute(
   }
 
   class { "naginator::compute_target": }
+
+  class { "quantum::quota":
+    quota_network             => $quantum_quota_network,
+    quota_subnet              => $quantum_quota_subnet,
+    quota_port                => $quantum_quota_port,
+    quota_router              => $quantum_quota_router,
+    quota_floatingip          => $quantum_quota_floatingip,
+    quota_security_group      => $quantum_quota_security_group,
+    quota_security_group_rule => $quantum_quota_security_group_rule,
+  }
 
 }
 
