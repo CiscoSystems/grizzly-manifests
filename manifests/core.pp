0c26b8212118ff67549a8437edd6efd385b3eee9
dotalton <dotalton@cisco.com>
2013-08-03 12:24:05 -0700

    added support for ceph-backed glance

--- manifests/core.pp
+++ manifests/core.pp
@@ -350,7 +350,13 @@ class control(
   }
   
   class { "coe::quantum_log": }
-  
+ 
+  if $::glance_ceph_enabled {
+    class { 'coe::ceph::control':
+      glance_store_user => $::glance_ceph_user,
+      glance_store_pool => $::glance_ceph_pool,
+    }
+  }
 }
 
 ### begin cinder standalone nodes
