6ac2f80a2be2669263192aaa9a979cf1558381a1
Pradeep Kilambi <pkilambi@cisco.com>
2013-08-23 15:28:21 -0400

    fix the ceph common to match class definition

--- manifests/core.pp
+++ manifests/core.pp
@@ -417,10 +417,7 @@ class ceph_common (
 class ceph_mon (
   $id
 ) {
-  class { 'ceph_common':
-    fsid      => $::ceph_monitor_fsid,
-    auth_type => $::ceph_auth_type,
-  }
+  class { 'ceph_common': }
   ceph::mon { $id:
     monitor_secret => $::ceph_monitor_secret,
     mon_port       => 6789,
